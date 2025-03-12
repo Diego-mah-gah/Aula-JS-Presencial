bba18f4ef55968e5a8af3b64a258705968b4afe9
Diego-mah-gah <kawadiego.soares@gmail.com>
2025-03-11 21:13:01 -0300

    Add files via upload

---
+++ ListaExerciciosAula2.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+let value = prompt ("Inform a number: ");
+let number = value*3;
+console.log("The value is: " + number);
---
+++ index.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+var name = prompt ("Insert your name: ");
+
+console.log("Hello, " + name + ". How have you been?");
+let ans = prompt("");
+console.log("I see... ")
+let age = prompt("Whats your age? ")
+console.log("Are you " + age + " yo?")
+console.log("Wow, that's nice! ")
+let color = prompt("What's your fav color? ")
+console.log("Wow, me too! We have so many things in common!");
+let film = prompt("What's your fav film? ")
+console.log(film + "It's a good film, but I do rather 1917");
+
